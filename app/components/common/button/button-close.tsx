c78387d5ae58127aec7dae770996a8a600db2426
ryuya yamamoto <ryuya.yamamoto@caters.co.jp>
2025-12-15 17:22:38 +0900

    [ update ] 削除ボタン スタイル調整

--- app/components/common/button/button-close.tsx
+++ app/components/common/button/button-close.tsx
@@ -17,7 +17,8 @@ const ButtonClose = ({
   ...props
 }: ButtonCloseProps) => {
   const style = cn(
-    "h-10 w-10 bg-primary rounded-full absolute cursor-pointer top-2 right-2 z-10 grid place-items-center",
+    "h-10 w-10 bg-primary rounded-full absolute cursor-pointer top-2 right-2 z-10 grid place-items-center outline-0",
+    "focus-visible:ring focus-visible:ring-white",
     "disabled:opacity-30 disabled:pointer-events-none",
     className
   );
