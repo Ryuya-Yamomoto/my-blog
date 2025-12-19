58cef804cdd7ee5c2feea223c1ca695a4dfa4992
ryuya yamamoto <ryuya.yamamoto@caters.co.jp>
2025-12-19 19:13:45 +0900

    [ update ] Basic認証 解除

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,34 @@
 import { NextResponse } from "next/server";
 
-export function middleware(req: Request) {
+export function middleware() {
   return NextResponse.next();
-
-  const basicAuth = req.headers.get("authorization");
-
-  // Production環境のみ適用
-  if (process.env.NODE_ENV !== "production") {
-    return NextResponse.next();
-  }
-
-  if (basicAuth) {
-    const auth = basicAuth.split(" ")[1];
-    const [user, pwd] = Buffer.from(auth, "base64").toString().split(":");
-
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      pwd === process.env.BASIC_AUTH_PASSWORD
-    ) {
-      return NextResponse.next();
-    }
-  }
-
-  return new NextResponse("Authentication required", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic realm="Secure Area"',
-    },
-  });
 }
+
+// ↓Basic認証を使用する場合はコメントアウト解除
+// export function middleware(req: Request) {
+//   const basicAuth = req.headers.get("authorization");
+
+//   // Production環境のみ適用
+//   if (process.env.NODE_ENV !== "production") {
+//     return NextResponse.next();
+//   }
+
+//   if (basicAuth) {
+//     const auth = basicAuth.split(" ")[1];
+//     const [user, pwd] = Buffer.from(auth, "base64").toString().split(":");
+
+//     if (
+//       user === process.env.BASIC_AUTH_USER &&
+//       pwd === process.env.BASIC_AUTH_PASSWORD
+//     ) {
+//       return NextResponse.next();
+//     }
+//   }
+
+//   return new NextResponse("Authentication required", {
+//     status: 401,
+//     headers: {
+//       "WWW-Authenticate": 'Basic realm="Secure Area"',
+//     },
+//   });
+// }
